10111a3ef5ac0dee3d569b46af1176ab1d51cc52
Anton Kenikh <kenikh@me.com>
2021-11-07 20:59:37 +0500

    Initial commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,328 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+
+contract Lottery is Ownable {
+  // add the library methods
+  using Counters for Counters.Counter;
+  using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+  enum State {
+    Inactive,
+    Active
+  }
+
+  // lottery state
+  State private state;
+
+  // participant data
+  struct Participant {
+    // total tickets purchased by participant
+    uint256 tickets; // default: 0
+  }
+
+  // enumerable map of participants addresses,
+  // represents index => address
+  EnumerableMap.UintToAddressMap private addresses;
+
+  // map of participants data,
+  // represents address => Participant
+  mapping(address => Participant) private participants;
+
+  // current participants counter
+  Counters.Counter private counter;
+
+  // manager (fee receiver)
+  address private manager;
+
+  // ticket price
+  uint256 private ticketPrice;
+
+  // max participatns, once the maximum number of participants has been reached,
+  // the lottery will be locked and a winner will have to be determined
+  uint256 private maxParticipants;
+
+  // max duration
+  uint256 private maxDuration;
+
+  // service fee
+  uint256 private fee;
+
+  // end time
+  uint256 private endTime;
+
+  // total tickets purchased
+  uint256 private tickets;
+
+  // bonded tokens
+  uint256 private tokens;
+
+  // strict/unlimited mode
+  // in strict mode the participant can only buy one ticket,
+  // otherwise any number is available for purchase
+  bool private strict;
+
+  constructor(
+    bool _strict,
+    uint256 _ticketPrice,
+    uint256 _maxParticipants,
+    uint256 _maxDuration,
+    uint256 _fee,
+    address _manager
+  ) {
+    require(_ticketPrice > 0, "INVALID_TICKET_PRICE");
+    state = State.Active;
+    strict = _strict;
+    ticketPrice = _ticketPrice;
+    maxParticipants = _maxParticipants;
+    maxDuration = _maxDuration;
+    fee = _fee;
+    manager = _manager;
+    endTime = block.timestamp + maxDuration;
+  }
+
+  // //////
+  // events
+  // //////
+
+  event LotteryDeactivated();
+  event SendCommission(uint256 amount, address to);
+  event SendRewards(uint256 amount, address to);
+
+  // ///////
+  // getters
+  // ///////
+
+  // get number of tickets for specific address
+  // =>
+
+  function getAddressTickets(address addr) external view returns (uint256) {
+    return participants[addr].tickets;
+  }
+
+  // get the lottery info
+  // =>
+
+  function getInfo()
+    external
+    view
+    returns (
+      // manager address
+      address,
+      // strict mode?
+      bool,
+      // ticket price
+      uint256,
+      // current number of participants
+      uint256,
+      // max number of participants
+      uint256,
+      // end time
+      uint256,
+      // max duration
+      uint256,
+      // total tickets purchased
+      uint256,
+      // bonded tokens
+      uint256,
+      // current state
+      State
+    )
+  {
+    return (
+      manager,
+      strict,
+      ticketPrice,
+      counter.current(),
+      maxParticipants,
+      endTime,
+      maxDuration,
+      tickets,
+      tokens,
+      state
+    );
+  }
+
+  // ///////
+  // helpers
+  // ///////
+
+  // generate pseudo random uint256
+  // =>
+
+  function pseudoRandom() private view returns (uint256) {
+    // 👀
+    return
+      uint256(
+        keccak256(
+          abi.encodePacked(
+            blockhash(block.number - 1),
+            block.coinbase,
+            block.difficulty,
+            tokens + tickets
+          )
+        )
+      );
+  }
+
+  // ///////////////
+  // private methods
+  // ///////////////
+
+  // pay reward
+  // =>
+
+  function payReward(address winner) private {
+    uint256 commission = (tokens * fee) / 10000;
+    uint256 reward = tokens - commission;
+
+    // pay commission
+    // =>
+
+    if (commission > 0) {
+      payable(manager).transfer(commission);
+      emit SendCommission(commission, manager);
+    }
+
+    // pay rewards
+    // =>
+
+    payable(winner).transfer(reward);
+    emit SendRewards(reward, winner);
+  }
+
+  // pick the winner
+  // =>
+
+  function pickWinner() private {
+    if (counter.current() == 0) {
+      restart();
+      return;
+    }
+
+    uint256 rnd = pseudoRandom() % (strict ? counter.current() : tickets);
+
+    if (strict) {
+      payReward(addresses.get(rnd));
+      restart();
+      return;
+    }
+
+    uint256 ticketsLooped = 0;
+
+    for (uint256 i = 0; i < counter.current(); i++) {
+      // check participatns tickets range
+      // rnd: 2
+      // 0: 1 ticket (1)
+      // 1: 2 tickets (2, 3) <= the winner
+      // 2: 1 ticket (4)
+      // ...
+      // =>
+
+      if (
+        rnd >= ticketsLooped + 1 &&
+        rnd <= ticketsLooped + participants[addresses.get(i)].tickets
+      ) {
+        payReward(addresses.get(i));
+        restart();
+        return;
+      }
+
+      ticketsLooped += participants[addresses.get(i)].tickets;
+    }
+
+    // will never be reached
+  }
+
+  // restart the lottery (reset state)
+  // =>
+
+  function restart() private {
+    for (uint256 i = 0; i < counter.current(); i++) {
+      delete participants[addresses.get(i)];
+      addresses.remove(i);
+    }
+
+    counter.reset();
+    tickets = 0;
+    tokens = 0;
+  }
+
+  // ////////////////
+  // external methods
+  // ////////////////
+
+  // enter to the lottery
+  // =>
+
+  function enter(uint256 ticketsToBePurchased) external payable {
+    // check state
+    // =>
+
+    require(state == State.Active, "INVALID_STATE");
+
+    // check tickets amount
+    // in strict mode the participant can only buy one ticket,
+    // otherwise any number is available for purchase
+    // =>
+
+    require(
+      strict ? ticketsToBePurchased == 1 : ticketsToBePurchased > 0,
+      "INCORRECT_TICKETS_AMOUNT"
+    );
+
+    // check tickets cost
+    // =>
+
+    require(
+      strict
+        ? msg.value == ticketPrice
+        : msg.value / ticketPrice == ticketsToBePurchased &&
+          msg.value % ticketPrice == 0,
+      "INCORRECT_TICKETS_COST"
+    );
+
+    // check the possibility of buying tickets
+    // =>
+
+    require(
+      !strict || participants[msg.sender].tickets == 0,
+      "IMPOSSIBLE_TO_BUY_MORE"
+    );
+
+    // add new participant
+    // =>
+
+    if (participants[msg.sender].tickets == 0) {
+      addresses.set(counter.current(), msg.sender);
+      counter.increment();
+    }
+
+    // buy tickets
+    // =>
+
+    participants[msg.sender].tickets += ticketsToBePurchased;
+    tickets += ticketsToBePurchased;
+    tokens += msg.value;
+
+    if (counter.current() == maxParticipants || block.timestamp >= endTime) {
+      pickWinner();
+    }
+  }
+
+  // terminate immediately
+  // =>
+
+  function terminate(bool deactivate) external onlyOwner {
+    pickWinner();
+
+    // 🔒
+    if (deactivate) {
+      state = State.Inactive;
+      emit LotteryDeactivated();
+    }
+  }
+}
---
+++ test/index.ts
@@ -0,0 +1,519 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { expect } from "chai";
+import { Signer } from "ethers";
+import { ethers } from "hardhat";
+
+describe("Lottery", function () {
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
+
+  let accounts: Signer[] = [];
+  let manager: string;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    accounts = await ethers.getSigners();
+    manager = await accounts[0].getAddress();
+  });
+
+  describe("Permissions", function () {
+    const strict = true;
+    const ticketPrice = ethers.utils.parseEther("1.0");
+    const maxParticipants = 5;
+    const maxDuration = 100;
+    const fee = 250;
+
+    it("Lottery termination by the owner should be successful", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const tx = await lottery.terminate(true);
+      const result = await tx.wait();
+
+      expect(result.events![0].event).to.equal("LotteryDeactivated");
+    });
+
+    it("Lottery termination by a non-owner should not be successful", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const tx = lottery.connect(accounts[1]).terminate(true);
+      await expect(tx).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+  });
+
+  describe("Information", function () {
+    const strict = true;
+    const ticketPrice = ethers.utils.parseEther("1.0");
+    const maxParticipants = 5;
+    const maxDuration = 100;
+    const fee = 250;
+
+    it("The lottery information to be returned should be the same as the one being initiated", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const information = await lottery.getInfo();
+
+      // manager address
+      expect(information[0]).to.equal(manager);
+
+      // strict mode?
+      expect(information[1]).to.equal(strict);
+
+      // ticket price
+      expect(information[2]).to.equal(ticketPrice);
+
+      // current number of participants
+      expect(information[3]).to.equal(0);
+
+      // max number of participants
+      expect(information[4]).to.equal(maxParticipants);
+
+      // TODO: end time
+      // expect(information[5]).to.equal(x + maxDuration);
+
+      // max duration
+      expect(information[6]).to.equal(maxDuration);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(0);
+
+      // bonded tokens
+      expect(information[8]).to.equal(0);
+
+      // current state
+      expect(information[9]).to.equal(1);
+    });
+  });
+
+  describe("Lottery I (strict)", function () {
+    const strict = true;
+    const ticketPrice = ethers.utils.parseEther("1.0");
+    const maxParticipants = 5;
+    const maxDuration = 100;
+    const fee = 250;
+
+    it("Should one player be successfully added to the lottery", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(1);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(1);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice);
+    });
+
+    it("Should two players be successfully added to the lottery", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      await lottery.connect(accounts[2]).enter(1, { value: ticketPrice });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(2);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(2);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice.mul(2));
+    });
+
+    it("Should not be able to enter with two or more tickets", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const tx = lottery
+        .connect(accounts[1])
+        .enter(2, { value: ticketPrice.mul(2) });
+      await expect(tx).to.be.revertedWith("INCORRECT_TICKETS_AMOUNT");
+    });
+
+    it("Should not be able to enter with an amount not equal to the ticket price", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const tx = lottery
+        .connect(accounts[1])
+        .enter(1, { value: ticketPrice.mul(2) });
+      await expect(tx).to.be.revertedWith("INCORRECT_TICKETS_COST");
+    });
+
+    it("Should not be able to enter twice", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      const tx = lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      await expect(tx).to.be.revertedWith("IMPOSSIBLE_TO_BUY_MORE");
+    });
+  });
+
+  describe("Lottery II (non-strict)", function () {
+    const strict = false;
+    const ticketPrice = ethers.utils.parseEther("1.0");
+    const maxParticipants = 5;
+    const maxDuration = 100;
+    const fee = 250;
+
+    it("Should one player be successfully added to the lottery", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(1);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(1);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice);
+    });
+
+    it("Should successfully add two players to the lottery", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      await lottery.connect(accounts[2]).enter(1, { value: ticketPrice });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(2);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(2);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice.mul(2));
+    });
+
+    it("Should be able to enter with two or more tickets", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery
+        .connect(accounts[1])
+        .enter(2, { value: ticketPrice.mul(2) });
+      await lottery
+        .connect(accounts[2])
+        .enter(3, { value: ticketPrice.mul(3) });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(2);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(5);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice.mul(5));
+    });
+
+    it("Should not be able to enter with an amount not equal to the ticket price", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      const tx = lottery
+        .connect(accounts[1])
+        .enter(1, { value: ticketPrice.mul(2) });
+      await expect(tx).to.be.revertedWith("INCORRECT_TICKETS_COST");
+    });
+
+    it("Should be able to enter twice", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+      const lottery = await Lottery.deploy(
+        strict,
+        ticketPrice,
+        maxParticipants,
+        maxDuration,
+        fee,
+        manager
+      );
+      await lottery.deployed();
+
+      await lottery.connect(accounts[1]).enter(1, { value: ticketPrice });
+      await lottery
+        .connect(accounts[1])
+        .enter(2, { value: ticketPrice.mul(2) });
+      const information = await lottery.getInfo();
+
+      // current number of participants
+      expect(information[3]).to.equal(1);
+
+      // total tickets purchased
+      expect(information[7]).to.equal(3);
+
+      // bonded tokens
+      expect(information[8]).to.equal(ticketPrice.mul(3));
+    });
+  });
+
+  // INFO: hardfork should be < london to test this
+  describe("Simulations", function () {
+    // strict
+    const l1 = {
+      strict: true,
+      ticketPrice: ethers.utils.parseEther("0.1"),
+      maxParticipants: 3,
+      maxDuration: 100,
+      fee: 250,
+      ticketsToBePurchased: 1,
+    };
+
+    // non-strict
+    const l2 = {
+      strict: false,
+      ticketPrice: ethers.utils.parseEther("0.55"),
+      maxParticipants: 7,
+      maxDuration: 100,
+      fee: 100,
+      ticketsToBePurchased: 3,
+    };
+
+    it("Should be passed", async function () {
+      const Lottery = await ethers.getContractFactory("Lottery");
+
+      const lottery1 = await Lottery.deploy(
+        l1.strict,
+        l1.ticketPrice,
+        l1.maxParticipants,
+        l1.maxDuration,
+        l1.fee,
+        manager
+      );
+      await lottery1.deployed();
+
+      const lottery2 = await Lottery.deploy(
+        l2.strict,
+        l2.ticketPrice,
+        l2.maxParticipants,
+        l2.maxDuration,
+        l2.fee,
+        manager
+      );
+      await lottery2.deployed();
+
+      // accounts
+      // =>
+
+      let gasUsed = BigNumber.from(0);
+      let initialSum = BigNumber.from(0);
+      let finalSum = BigNumber.from(0);
+
+      // owner
+      // =>
+
+      let initialBalance = BigNumber.from(0);
+      let totalFee = BigNumber.from(0);
+
+      // participants
+      // =>
+
+      const count = accounts.length - 1;
+
+      // initial values
+      // =>
+
+      for (let i = 0; i <= count; i++) {
+        const x = accounts[i];
+        const balance = await x.getBalance();
+
+        if (i === 0) {
+          initialBalance = initialBalance.add(balance);
+        } else {
+          initialSum = initialSum.add(balance);
+        }
+      }
+
+      // simmulate
+      // =>
+
+      for (let i = 1; i <= count; i++) {
+        const x = accounts[i];
+
+        // strict
+        const tx1 = await lottery1.connect(x).enter(l1.ticketsToBePurchased, {
+          value: l1.ticketPrice.mul(l1.ticketsToBePurchased),
+        });
+
+        // non-strict
+        const tx2 = await lottery2.connect(x).enter(l2.ticketsToBePurchased, {
+          value: l2.ticketPrice.mul(l2.ticketsToBePurchased),
+        });
+
+        const r1 = await tx1.wait();
+        const r2 = await tx2.wait();
+
+        gasUsed = gasUsed.add(r1.gasUsed);
+        gasUsed = gasUsed.add(r2.gasUsed);
+      }
+
+      // check
+      // =>
+
+      const information1 = await lottery1.getInfo();
+      const information2 = await lottery2.getInfo();
+
+      // the number of participants remaining in the lottery1
+      const v1 = count % l1.maxParticipants;
+
+      // current number of participants
+      expect(information1[3]).to.equal(v1);
+
+      // bonded tokens
+      expect(information1[8]).to.equal(
+        l1.ticketPrice.mul(v1).mul(l1.ticketsToBePurchased)
+      );
+
+      // the number of participants remaining in the lottery2
+      const v2 = count % l2.maxParticipants;
+
+      // current number of participants
+      expect(information2[3]).to.equal(v2);
+
+      // bonded tokens
+      expect(information2[8]).to.equal(
+        l2.ticketPrice.mul(v2).mul(l2.ticketsToBePurchased)
+      );
+
+      // final values
+      // =>
+
+      for (let i = 0; i <= count; i++) {
+        const x = accounts[i];
+        const balance = await x.getBalance();
+
+        if (i === 0) {
+          // total fee received by the manager
+          totalFee = balance.sub(initialBalance);
+        } else {
+          // the sum of all balances of the participants
+          finalSum = finalSum.add(balance);
+        }
+      }
+
+      expect(finalSum).to.equal(
+        initialSum
+          // subtract bonded tokens remaining in lottery1
+          .sub(information1[8])
+          // subtract bonded tokens remaining in lottery2
+          .sub(information2[8])
+          // subtract tx fee (gasPrice = 1, hardfork < london required for accuracy)
+          .sub(gasUsed)
+          // subtract total fee received by the manager
+          .sub(totalFee)
+      );
+    });
+  });
+});
